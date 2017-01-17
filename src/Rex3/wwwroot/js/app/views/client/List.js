16dc6b18aa5ae51747d421c601fefb885dbf9dc3
chrispy632 <chris.pickering@xerox.com>
2017-01-17 13:13:20 +0000

    added base layout

---
+++ src/Rex3/wwwroot/js/app/views/client/List.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    $("#clientList").kendoGrid(
+        {
+            toolbar: ["pdf"],
+            pdf: {
+                fileName: "Account Grid Export.pdf"
+            },
+            dataSource: {
+                transport: {
+                    read: { url: '/Client/GetClients', type: 'GET' },
+                    total: function (response) {
+                        return (response.data).length;
+                    }
+                }
+            },
+            height: 550,
+            groupable: true,
+            sortable: true,
+            pageable: {
+                    refresh: true,
+                    pageSizes: true,
+                    buttonCount: 5
+            },
+            columns: [
+                { field: "accountId", title: "System ID", width: 150 },
+                { field: "accountName", title: "Company Name" },
+                { field: "AccountNameAka", title: "Alternate Name", width: 150 }]
+    });
+});
